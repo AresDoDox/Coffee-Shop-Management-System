4e820789068370dad324f3f12b99de25ca7ccd6e
ThaiDTC <thaidtc@bap.jp>
2026-01-23 09:19:59 +0700

    [FE][Phase 2] Realtime - Notify Kitchen

--- coffee-shop-frontend/src/App.tsx
+++ coffee-shop-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import PosPage from './pages/PosPage';
+import KitchenPage from './pages/KitchenPage';
 import PrivateRoute from './layouts/PrivateRoute';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/pos" element={<PosPage />} />
+          <Route path="/kitchen" element={<KitchenPage />} />
         </Route>
 
         {/* Public Routes */}
